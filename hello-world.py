dc1ce77bdce2e5b43f6ef039d2b241ad6cc6d015
Kai-Agueros05 <kagueros@ucsc.edu>
2025-04-01 15:06:27 -0700

    Add files via upload

---
+++ hello-world.py
@@ -0,0 +1,20 @@
+#This is a way to write notes!!
+#This program will write "hello world!
+
+#tells computer to print HelloWorld
+def PrintHelloWorld():
+	print("Hello World")
+	pass
+
+#Defines our main()
+#function for our program
+def main():
+	PrintHelloWorld()
+	pass
+
+
+#When we run the program
+#this executes first
+if __name__ == '__main__':
+	main()
+
