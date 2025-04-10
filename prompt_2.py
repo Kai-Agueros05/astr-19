c9b9ea1e5d8fbe96dfd7a431927b014028c521cf
Kai-Agueros05 <kagueros@ucsc.edu>
2025-04-10 15:41:42 -0700

    Add files via upload

---
+++ prompt_2.py
@@ -0,0 +1,26 @@
+def main():
+	#sum of two floating numbers
+	float1 = 3.9
+	float2 = 9.4
+	sum_floats = float1 + float2
+	print("Sum is ", sum_floats)
+	print("Type of data is a ", type(sum_floats))
+
+	#difference of two integers
+	int1 = 4
+	int2 = 1
+	diff_ints = int1 - int2
+	print("Difference is ", diff_ints)
+	print("Type of data is a ", type(diff_ints))
+
+	#product of a floating point number and an integer
+	float3 = 8.45
+	int3 = 5
+	prod = float3 * int3
+	print("Product is", prod)
+	print("Type of data is a ", type(prod))
+
+
+if __name__ == '__main__':
+	main()
+	
